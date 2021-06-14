27b5432ecfa47850f7df2a52f15eee1dffcfd1b9
parksil0 <parksil00@naver.com>
2021-06-15 00:45:18 +0900

    화살표함수와 일반 함수에서 this가 어떻게 작동되고, 어떤 차이가 있는지 확인하는 예제 추가

--- blog_code/js_this_example.js
+++ blog_code/js_this_example.js
@@ -29,3 +29,30 @@ let myNameObj = {
 }
 
 myNameObj.greeting(); // hi! Slias Park
+
+
+/*
+ * 화살표함수와 일반 함수간의 차이점을 알아보자.
+ */
+
+// 프로필 객체를 만든다.
+let myProfile = {
+  age: 28,
+  name: 'Seal Park'
+}
+
+// 이름을 출력하는 함수를 생성하였다.
+const printAge = function() {
+  console.log(this.age);
+}
+
+// 이름을 출력하는 함수지만 화살표 함수로 생성하였다.
+const printAgeArrow = () => console.log(this.age);
+
+myProfile.printAge = printAge;
+
+myProfile.printAge(); // 28
+
+myProfile.printAgeArrow = printAgeArrow;
+
+myProfile.printAgeArrow(); // undefined
